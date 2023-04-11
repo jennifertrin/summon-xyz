158ac9387dcd9edf1f5d80bfee23d17fb7f4bf99
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-04-11 16:09:38 -0700

    inital next setup (#1)

---
+++ app/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Cta from "@/components/cta";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <Cta />
+    </main>
+  )
+}
