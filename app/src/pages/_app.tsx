e8915f09e01f05787dc920de2429a9d79f725809
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-04-11 23:13:07 -0700

    chore: move dynamic context provider

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId:
+          process.env.NODE_ENV == "development"
+            ? (process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID_DEV as string)
+            : (process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID_PROD as string),
+      }}
+    >
+      <Component {...pageProps} />
+    </DynamicContextProvider>
+  );
 }
