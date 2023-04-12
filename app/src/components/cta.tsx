1f3455f93c27c959d464ae7a4429385b0971858e
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-04-11 17:37:42 -0700

    add dynamic wallet sdk (#2)

--- app/src/components/cta.tsx
+++ app/src/components/cta.tsx
@@ -1,4 +1,5 @@
-import 'flowbite';
+import "flowbite";
+import DynamicContextProvider from "./ContextProvider/DynamicContextProvider";
 
 export default function Cta() {
   return (
@@ -12,12 +13,9 @@ export default function Cta() {
             A tool that enhances web3 games with analytics and community-built
             gaming experiences.
           </p>
-          <a
-            href="#"
-            className="text-white bg-primary hover:bg-primary focus:ring-4 focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary focus:outline-none dark:focus:ring-primary"
-          >
-            Start Now
-          </a>
+          <div className="inline-block mx-auto">
+            <DynamicContextProvider />
+          </div>
         </div>
       </div>
     </section>
