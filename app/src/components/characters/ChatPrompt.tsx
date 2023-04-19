e70d582298dfdfecec95a37df5fe82b7d0ff89e8
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-04-18 18:34:15 -0700

    chore: mint button as coming soon

--- app/src/components/characters/ChatPrompt.tsx
+++ app/src/components/characters/ChatPrompt.tsx
@@ -148,10 +148,11 @@ function App({ conversation }: App) {
                     Confirm and post design
                   </button>
                   <button
+                    disabled={true}
                     type="button"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    className="text-white bg-blue-700 disabled:bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                   >
-                    Mint design
+                    Mint design (Comming Soon!)
                   </button>
                 </div>
               </div>
